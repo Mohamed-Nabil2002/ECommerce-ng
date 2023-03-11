5a511eed80da4f28eb4239a0c740b04b00155416
mohamednabil <mohamed.nabil@lagorta.com>
2023-03-11 20:35:57 +0200

    Make home and seller-auth component and make route

--- ng-ECommerce/src/app/app-routing.module.ts
+++ ng-ECommerce/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "seller-auth",
+    component: SellerAuthComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ng-ECommerce/src/app/app.module.ts
+++ ng-ECommerce/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    HomeComponent,
+    SellerAuthComponent
   ],
   imports: [
     BrowserModule,
