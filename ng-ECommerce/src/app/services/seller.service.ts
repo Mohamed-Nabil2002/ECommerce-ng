201235964517425f43af935152db38f0f78b2882
mohamednabil <mohamed.nabil@lagorta.com>
2023-03-19 10:28:42 +0200

    Add Auth guard to routing

--- ng-ECommerce/src/app/services/seller.service.ts
+++ ng-ECommerce/src/app/services/seller.service.ts
@@ -1,16 +1,33 @@
+import { Router } from '@angular/router';
 import { SignUp } from './../interfaces/data-type';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SellerService {
+  isSellerLoggedIn = new BehaviorSubject<boolean>(false);
+  constructor(private http: HttpClient,
+    private router: Router) { }
 
-  constructor(private http: HttpClient) { }
+  userSignUp(data: SignUp) {
+    this.http
+      .post('http://localhost:3000/seller', data, { observe: 'response' })
+      .subscribe(result => {
+        this.isSellerLoggedIn.next(true)
+        localStorage.setItem("seller", JSON.stringify(result.body))
+        this.router.navigate(['seller-home'])
+      });
+    return false
+  }
 
-    userSignUp(data: SignUp) {
-      return this.http.post('http://localhost:3000/seller', data)
+  reloadSeller() {
+    if (localStorage.getItem('seller')) {
+      this.isSellerLoggedIn.next(true)
+      this.router.navigate(['seller-home'])
     }
+  }
 
 }
